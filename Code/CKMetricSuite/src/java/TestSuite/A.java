a0c43062c7be6806f7c09a80bda7edf6319806c0
Amar Kumar <amar_kumar@hotmail.com>
2017-03-19 02:27:05 -0400

    Create A.java

---
+++ Code/CKMetricSuite/src/java/TestSuite/A.java
@@ -0,0 +1,26 @@
+package TestSuite;
+import TestSuite.*;
+
+public class A  {
+	private int Attribute3;
+	private String Attribute1;
+	private int Attribute2;
+	
+	public void Method2()
+	{
+		
+	}
+	
+	public void Method3()
+	{
+	}
+	
+	public void Method4()
+	{
+		
+	}
+	
+	public void Method1(E es)
+	{
+	}
+}
